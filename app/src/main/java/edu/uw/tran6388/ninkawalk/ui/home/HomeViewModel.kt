c63c28414e239463e15b42fa3cba42b83d2bfef6
joon1998 <joon1998@uw.edu>
2019-11-26 12:17:11 -0800

    Add basic designs

--- app/src/main/java/edu/uw/tran6388/ninkawalk/ui/home/HomeViewModel.kt
+++ app/src/main/java/edu/uw/tran6388/ninkawalk/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Walk to buy cool stuff from the shop!"
     }
     val text: LiveData<String> = _text
 }
