eb540f45ac455d444c2143bc5d5d1c24b896a367
joon1998 <joon1998@uw.edu>
2019-12-03 16:24:50 -0800

    misc

--- app/src/main/java/edu/uw/tran6388/ninkawalk/MainActivity.kt
+++ app/src/main/java/edu/uw/tran6388/ninkawalk/MainActivity.kt
@@ -1,20 +1,35 @@
 package edu.uw.tran6388.ninkawalk
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.fragment_dashboard.*
+import kotlinx.android.synthetic.main.fragment_home.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
+
+    var running = false
+    var sensorManager:SensorManager? = null
+    var totalPoints: String? = "0"
+    //private var query: String? = ""
+    private val QUERY_STRING: String = "search query"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
@@ -25,5 +40,42 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        findViewById<TextView>(R.id.text_display).text = "0"
+        //text_display.text = "0"
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putString(QUERY_STRING, totalPoints)
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onResume() {
+
+        super.onResume()
+        running = true
+        var stepsSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        if (stepsSensor == null) {
+            //Toast.makeText(this, "No Step Counter Sensor !", Toast.LENGTH_SHORT).show()
+        } else {
+            sensorManager?.registerListener(this, stepsSensor, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        running = false
+        sensorManager?.unregisterListener(this)
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (running) {
+            text_display.text = "" + event.values[0].toInt()
+            totalPoints = event.values[0].toInt().toString()
+        }
+    }
+
 }
--- app/src/main/java/edu/uw/tran6388/ninkawalk/ui/home/HomeFragment.kt
+++ app/src/main/java/edu/uw/tran6388/ninkawalk/ui/home/HomeFragment.kt
@@ -1,5 +1,6 @@
 package edu.uw.tran6388.ninkawalk.ui.home
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -16,8 +17,10 @@ import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.widget.Toast
+import androidx.core.content.ContextCompat.getSystemService
+import kotlinx.android.synthetic.main.fragment_home.*
 
-class HomeFragment : Fragment(), SensorEventListener {
+class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
 
@@ -28,6 +31,7 @@ class HomeFragment : Fragment(), SensorEventListener {
     ): View? {
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        //sensorManager = getSystemService(this.SENSOR_SERVICE) as SensorManager
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(this, Observer {
@@ -36,30 +40,4 @@ class HomeFragment : Fragment(), SensorEventListener {
         return root
     }
 
-    override fun onResume() {
-        super.onResume()
-        running = true
-        var stepsSensor = sensorManager?.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
-
-        if (stepsSensor == null) {
-            Toast.makeText(this, "No Step Counter Sensor !", Toast.LENGTH_SHORT).show()
-        } else {
-            sensorManager?.registerListener(this, stepsSensor, SensorManager.SENSOR_DELAY_UI)
-        }
-    }
-
-    override fun onPause() {
-        super.onPause()
-        running = false
-        sensorManager?.unregisterListener(this)
-    }
-
-    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
-    }
-
-    override fun onSensorChanged(event: SensorEvent) {
-        if (running) {
-            stepsValue.setText("" + event.values[0])
-        }
-    }
 }
